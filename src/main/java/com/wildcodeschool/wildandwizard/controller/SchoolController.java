951b77454c34e57748f3098430a2b1cf24237d3a
Thibault-Povedano <thibault@mbp-de-thibault.home>
2022-07-27 10:16:04 +0200

    first commit

---
+++ src/main/java/com/wildcodeschool/wildandwizard/controller/SchoolController.java
@@ -0,0 +1,56 @@
+package com.wildcodeschool.wildandwizard.controller;
+
+import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.repository.SchoolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class SchoolController {
+    @Autowired  // TODO : get school repository by dependency injection
+    private SchoolRepository repository;
+    @GetMapping("/schools")
+    public String getAll(Model model) {
+        model.addAttribute("schools", repository.findAll());
+        // TODO : find all schools
+        return "schools";
+    }
+
+    @GetMapping("/school")
+    public String getSchool(Model model,
+                            @RequestParam(required = false) Long id) {
+        School school= new School();
+        if(id != null) {
+        Optional<School> optionalSchool = repository.findById(id);
+        if(optionalSchool.isPresent()){
+            school = optionalSchool.get();
+        }
+    }
+    model.addAttribute("school", school);
+        // TODO : find one school by id
+
+        return "school";
+    }
+
+    @PostMapping("/school")
+    public String postSchool(@ModelAttribute School school) {
+        repository.save(school);
+        return "redirect:/schools";
+        // TODO : create or update a school
+    }
+
+    @GetMapping("/school/delete")
+    public String deleteSchool(@RequestParam Long id) {
+        repository.deleteById(id);
+        // TODO : delete a school
+
+        return "redirect:/schools";
+    }
+}
